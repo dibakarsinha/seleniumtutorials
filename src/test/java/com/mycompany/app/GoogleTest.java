c2895c016de2f62b4659128d14dd6f102d28be6f
dibakarsinha <dibakar18@gmail.com>
2026-01-31 19:01:45 +0530

    added headless ci modification

--- src/test/java/com/mycompany/app/GoogleTest.java
+++ src/test/java/com/mycompany/app/GoogleTest.java
@@ -3,14 +3,26 @@ package com.mycompany.app;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GoogleTest {
 
     @Test
-    void openGoogleTest() {
-        WebDriver driver = new ChromeDriver();
+    void openGoogleInHeadlessMode() {
+
+       boolean isHeadless = Boolean.parseBoolean(
+        System.getProperty("headless", "false"));
+
+ChromeOptions options = new ChromeOptions();
+if (isHeadless) {
+    options.addArguments("--headless=new");
+}
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
 
         assertEquals("Google", driver.getTitle());
